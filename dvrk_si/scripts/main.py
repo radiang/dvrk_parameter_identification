00a5dcc2366ec4c18b9cdf8df6e786edf0d9ad03
Radian Azhar <radiang91@gmail.com>
2018-04-21 15:42:29 -0400

    new data, failed effort test

--- dvrk_si/scripts/main.py
+++ dvrk_si/scripts/main.py
@@ -13,27 +13,31 @@ testname =  'test_position'
 q  = genfromtxt(foldername+testname+'.csv', delimiter=',')
 qt = q.transpose()
 
-times = 2; 
+times = 1
 
-a = np.zeros((len(q[:][1]),len(q[1][:])*times))
+if times ==1: 
+	a = q;
+else:	
+	a = np.zeros((len(q[:][1]),len(q[1][:])*times))
 
-#Make twice the trajectory
-for i in range(len(qt[1][:])):
-	a[i][:]=np.append(q[i][:],q[i][:])
+	#Make twice the trajectory
+	for i in range(len(qt[1][:])):
+		a[i][:]=np.append(q[i][:],q[i][:])
 
 p=dvrk.psm('PSM1')
-r=rospy.Rate(2/0.01/1.5)
+r=rospy.Rate(2/0.01*0.75)
 p.home()
 p.move_joint_some(np.array([0.0,0.0,0.0]),np.array([0,1,2]))
 
 states = np.zeros((len(a[1][:]),3*3))
 
-i = 0;
+i = 0
+scale = 0.92
 
 while  i<len(a[1][:]) and not rospy.is_shutdown():
 #for i in range(len(q[1][:])):
 	#print(qt[1])
-	p.move_joint_some(np.array([a[0][i], a[1][i], a[2][i]]),np.array([0,1,2]),False)
+	p.move_joint_some(np.array([scale*a[0][i], scale*a[1][i], scale*a[2][i]]),np.array([0,1,2]),False)
 	states[i][0:3] = p.get_current_joint_position()[0:3]
 	states[i][3:6] = p.get_current_joint_velocity()[0:3]
 	states[i][6:9] = p.get_current_joint_effort()[0:3]
@@ -41,7 +45,7 @@ while  i<len(a[1][:]) and not rospy.is_shutdown():
 
 	i = i +1
 	
-with open(foldername+'PID_data_twice_.75speed.csv', 'wb') as myfile:
+with open(foldername+'PID_data_.75speed_try2.csv', 'wb') as myfile:
     wr = csv.writer(myfile, quoting=csv.QUOTE_NONE)
     for i in range(np.size(states,0)-10):
       wr.writerow(states[i])
--- dvrk_si/scripts/main_effort.py
+++ dvrk_si/scripts/main_effort.py
@@ -1,30 +1,42 @@
 #!/usr/bin/env python
 from numpy import genfromtxt
+import rospy
+import math
+import time
+import csv
 import numpy as np
 import dvrk
 
 
 foldername = './data/3dof_inplanepitch_svd_traj/'
-testname =  'PID_data.csv'
+testname =  'PID_data_.75speed_try2.csv'
 
 q= genfromtxt(foldername+testname, delimiter=',')
 qt= q.transpose()
 
-p=dvrk.psm('PSM2')
+hz = 2/0.01*0.75;
+
+p=dvrk.psm('PSM1')
 r=rospy.Rate(hz)
 p.home()
 p.move_joint_some(np.array([0.0,0.0,0.0]),np.array([0,1,2]))
 
-data = np.zeroes(len(states[6][:]),3*3)
+data = np.zeros((len(q[2][:]),3*3))
 
-for i in range(len(states[6][:])):
-	p.set_effort_joint(np.array([q[0][i], q[1][i], q[2][i], 0.0, 0.0, 0.0, 0.0]))
+i = 0
+scale = 1
+
+while  i<len(q[1][:]) and not rospy.is_shutdown():
+#for i in range(len(states[6][:])):
+	p.set_effort_joint(np.array([scale*q[6][i], scale*q[7][i], scale*q[8][i], 0.0, 0.0, 0.0, 0.0]))
 	
 	data[i][0:3] = p.get_current_joint_position()[0:3]
 	data[i][3:6] = p.get_current_joint_velocity()[0:3]
 	data[i][6:9] = p.get_current_joint_effort()[0:3]
 
-	r.Rate
+	r.sleep()
+	i=i+1
+
 #Print Code
 with open(foldername+'effort_data.csv', 'wb') as myfile:
     wr = csv.writer(myfile, quoting=csv.QUOTE_NONE)

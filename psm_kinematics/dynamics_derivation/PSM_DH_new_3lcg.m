f58c1a35d5846715eeb4302b4d11a6abb0d7263e
Radian A G <radiang91@gmail.com>
2018-04-26 11:46:41 -0400

    object

--- psm_kinematics/dynamics_derivation/PSM_DH_new_3lcg.m
+++ psm_kinematics/dynamics_derivation/PSM_DH_new_3lcg.m
@@ -3,31 +3,31 @@ close all
 
 syms T Jw lc1 lc2 lc3 lc4 lc5 lc6 lc7 lc8 lc9 l2 l3 l4 l5 l6 l7 l8 l9 m1 m2 m3 m4 m5 m6 m7 m8 m9 m10 m11 q1 q2 q3 q4 q5 q6 qd1 qd2 qd3 qd4 qd5 qd6  qdd1 qdd2 qdd3 qdd4 qdd5 qdd6 thet1 psi  tau1 tau2 tau3 tau4 tau5 tau6 real; 
 
-q   = [q1 q2 q3 q4 q5 q6];
-qd  = [qd1 qd2 qd3 qd4 qd5 qd6];
-qdd = [qdd1 qdd2 qdd3 qdd4 qdd5 qdd6];
+gen.q   = [q1 q2 q3 q4 q5 q6];
+gen.qd  = [qd1 qd2 qd3 qd4 qd5 qd6];
+gen.qdd = [qdd1 qdd2 qdd3 qdd4 qdd5 qdd6];
 
-Q = [q1 q2 q3 q4 q5 q6]';
-Qd = [qd1 qd2 qd3 qd4 qd5 qd6]';
-Qdd = [qdd1 qdd2 qdd3 qdd4 qdd5 qdd6]';
-Tau = [tau1 tau2 tau3 tau4 tau5 tau6]';
+gen.Q = [q1 q2 q3 q4 q5 q6]';
+gen.Qd = [qd1 qd2 qd3 qd4 qd5 qd6]';
+gen.Qdd = [qdd1 qdd2 qdd3 qdd4 qdd5 qdd6]';
+gen.Tau = [tau1 tau2 tau3 tau4 tau5 tau6]';
 
-M=[m1 m2 m3 m4 m5 m6 m7 m8 m9 m10 m11];
-Lc=[lc1 lc2 lc3 lc4 lc5 lc6 lc7 lc8 lc9];
+dyn.M=[m1 m2 m3 m4 m5 m6 m7 m8 m9 m10 m11];
+dyn.Lc=[lc1 lc2 lc3 lc4 lc5 lc6 lc7 lc8 lc9];
 
 
-filename='data/new_3dof_inaxis_svd';
+gen.filename='new_3dof_inaxis_svd';
 
 
 %% Options 
 %Degrees of Freedom of robot
-dof = 3; 
+gen.dof = 3; 
 
 %calculate dynamics?? 
-dynamic = 1;
+dyn.dynamic = 1;
 
 %Gravity 
-g =9.8; %m/s^2
+dyn.g =9.8; %m/s^2
 
 %% Try Plotting to check transformations
 %Joint Angles
@@ -37,14 +37,14 @@ q_n = [0, .5, .02, 0, 0, 0];  %Put your numeric values here
 lc_n = [-.03, .150/2, .516/2, .2881/2, .4162/2, .001, .001, .001]; %Put numeric values of Cg locations
 
 %% DH Paremeters and Transforms of PSM 
-p = sym(pi()); 
+dyn.p = sym(pi()); 
 %My own DH from RViz and DH Paper
-beta=atan((.072613-.0296)/(.2961-.1524));
+dyn.beta=atan((.072613-.0296)/(.2961-.1524));
 
-a      =    [ 0     ,        0,     .150,        .516,    .2881, -.0430,        0,      0,     .0091,    .0102,    0];
-alpha  =    [-p/2   ,     -p/2,        0,           0,        0,    p/2,        0,     p/2,     -p/2,        0, -p/2];
-d      =    [ 0.1524,    .0296,        0,           0,        0,      0, .4162+q3,       0,        0,        0,    0];
-tet    =    [ p/2   ,  -p/2+q1, -beta+q2, beta+p/2-q2,  -p/2+q2,   -p/2,      p/2,  p/2+q4,   p/2+q5,       q6, -p/2];
+dyn.a      =    [ 0     ,        0,     .150,        .516,    .2881, -.0430,        0,      0,     .0091,    .0102,    0];
+dyn.alpha  =    [-dyn.p/2   ,     -dyn.p/2,        0,           0,        0,    dyn.p/2,        0,     dyn.p/2,     -dyn.p/2,        0, -dyn.p/2];
+dyn.d      =    [ 0.1524,    .0296,        0,           0,        0,      0, .4162+gen.q(3),       0,        0,        0,    0];
+dyn.tet    =    [ dyn.p/2   ,  -dyn.p/2+gen.q(1), -dyn.beta+gen.q(2), dyn.beta+dyn.p/2-gen.q(2),  -dyn.p/2+gen.q(2),   -dyn.p/2,      dyn.p/2,  dyn.p/2+gen.q(4),   dyn.p/2+gen.q(5),       gen.q(6), -dyn.p/2];
 
 %1: Yaw Coordinate Frame
 %2-4: Pitch Coordinate Frames
@@ -56,20 +56,17 @@ tet    =    [ p/2   ,  -p/2+q1, -beta+q2, beta+p/2-q2,  -p/2+q2,   -p/2,      p/
 % 12: Counterweight Pitch
 % 13: Counterweight Insertion
 
-for i=1:length(a)
-Ti_i(1:4,1:4,i) = DH(tet(i),a(i),d(i),alpha(i));
+for i=1:length(dyn.a)
+dyn.Ti_i(1:4,1:4,i) = DH(dyn.tet(i),dyn.a(i),dyn.d(i),dyn.alpha(i));
 %Ti_i(1:4,1:4,i) = dh_modified(tet(i),a(i),d(i),alpha(i));
 end
 
-
-k=[0;0;1];
-
-for i=1:length(a)
+for i=1:length(dyn.a)
     if i==1
-        T(1:4,1:4,i) = Ti_i(:,:,i);
+        dyn.T(1:4,1:4,i) = dyn.Ti_i(:,:,i);
     
     else
-    T(1:4,1:4,i)=T(1:4,1:4,i-1)*Ti_i(1:4,1:4,i);
+    dyn.T(1:4,1:4,i)=dyn.T(1:4,1:4,i-1)*dyn.Ti_i(1:4,1:4,i);
     %T(1:4,1:4,j)=vpa(T(1:4,1:4,j),4);
     end
 end
@@ -83,11 +80,11 @@ d_c = [0];
 tet_c = [0];
 
 for i = 1:length(a_c)
-Ti_counter(:,:,i) = DH(tet_c(i),a_c(i),d_c(i),alpha_c(i)); 
+dyn.Ti_counter(:,:,i) = DH(tet_c(i),a_c(i),d_c(i),alpha_c(i)); 
 if i==1 
-    T_c(:,:,i)=Ti_counter(:,:,i);
+    dyn.T_c(:,:,i)=dyn.Ti_counter(:,:,i);
 else 
-    T_c(:,:,i)=T_c(:,:,i-1)*Ti_counter(:,:,i);
+    dyn.T_c(:,:,i)=dyn.T_c(:,:,i-1)*dyn.Ti_counter(:,:,i);
 end 
 end 
 
@@ -112,23 +109,23 @@ end
 % 
 %% Syms Stuff
 
-num = 11;
+dyn.num = 11;
 
-l_cg = sym('l_cg%d_%d', [num 3], 'real');
-l_cg = [l_cg,ones(num,1)];
+dyn.l_cg = sym('l_cg%d_%d', [dyn.num 3], 'real');
+dyn.l_cg = [dyn.l_cg,ones(dyn.num,1)];
 %Inertia Matrix notation
-Ixx = sym('I%d_xx', [11 1],'real');
-Iyy = sym('I%d_yy', [11 1],'real');
-Izz = sym('I%d_zz', [11 1],'real');
-Ixy = sym('I%d_xy', [11 1],'real');
-Ixz = sym('I%d_xz', [11 1],'real');
-Iyz = sym('I%d_yz', [11 1],'real');
+dyn.Ixx = sym('I%d_xx', [dyn.num 1],'real');
+dyn.Iyy = sym('I%d_yy', [dyn.num 1],'real');
+dyn.Izz = sym('I%d_zz', [dyn.num 1],'real');
+dyn.Ixy = sym('I%d_xy', [dyn.num 1],'real');
+dyn.Ixz = sym('I%d_xz', [dyn.num 1],'real');
+dyn.Iyz = sym('I%d_yz', [dyn.num 1],'real');
 
-for i=1:1:length(Ixx)
+for i=1:1:length(dyn.Ixx)
     
-    I(1:3,1:3,i)=[Ixx(i),Ixy(i),Ixz(i); 
-              Ixy(i),Iyy(i),Iyz(i);
-              Ixz(i),Iyz(i),Izz(i);]; 
+    dyn.I(1:3,1:3,i)=[dyn.Ixx(i),dyn.Ixy(i),dyn.Ixz(i); 
+              dyn.Ixy(i),dyn.Iyy(i),dyn.Iyz(i);
+              dyn.Ixz(i),dyn.Iyz(i),dyn.Izz(i);]; 
 end
 
 %% Make Stuff Zero
@@ -153,66 +150,66 @@ end
 % l_cg(6,1)=0;
 % l_cg(1,2)=0;
 %Lump Gripper stuff 
-M(7)=0;
-I(1:3,1:3,7) = zeros(3);
-l_cg(7,:) = zeros(1,4);
+dyn.M(7)=0;
+dyn.I(1:3,1:3,7) = zeros(3);
+dyn.l_cg(7,:) = zeros(1,4);
 
-M(8)=0;
-I(1:3,1:3,8) = zeros(3);
-l_cg(8,:) = zeros(1,4);
+dyn.M(8)=0;
+dyn.I(1:3,1:3,8) = zeros(3);
+dyn.l_cg(8,:) = zeros(1,4);
 
 %Lump More Stuff
 % M(6)=0;
 % I(1:3,1:3,6) = zeros(3);
 % l_cg(6,:) = zeros(1,4);
 
-M(9)=0;
-I(1:3,1:3,9) = zeros(3);
-l_cg(9,:) = zeros(1,4);
+dyn.M(9)=0;
+dyn.I(1:3,1:3,9) = zeros(3);
+dyn.l_cg(9,:) = zeros(1,4);
 
 
 %Assume counterweight pitch goes to zero 
-M(10)=0;
-I(1:3,1:3,10) = zeros(3);
-l_cg(10,:) = zeros(1,4);
+dyn.M(10)=0;
+dyn.I(1:3,1:3,10) = zeros(3);
+dyn.l_cg(10,:) = zeros(1,4);
 
 % M(11)=0;
 % I(1:3,1:3,11) = zeros(3);
 % l_cg(11,:) = zeros(1,4);
 
 %In Axis Masses
- l_cg(1,3) = 0;
- l_cg(1,2) = d(2)/2;
- l_cg(2,2:3) = 0;
- l_cg(3,2:3) = 0;
- l_cg(4,2:3) = 0;
- l_cg(6,1:2) = 0;
- l_cg(7,1) = 0; 
- l_cg(7,3) = 0; 
- l_cg(8,2:3) = 0;
- l_cg(9,2:3) = 0;
- l_cg(11,1:2) = 0;
+ dyn.l_cg(1,3) = 0;
+ dyn.l_cg(1,2) = dyn.d(2)/2;
+ dyn.l_cg(2,2:3) = 0;
+ dyn.l_cg(3,2:3) = 0;
+ dyn.l_cg(4,2:3) = 0;
+ dyn.l_cg(6,1:2) = 0;
+ dyn.l_cg(7,1) = 0; 
+ dyn.l_cg(7,3) = 0; 
+ dyn.l_cg(8,2:3) = 0;
+ dyn.l_cg(9,2:3) = 0;
+ dyn.l_cg(11,1:2) = 0;
 
 
 %% Let's try a new Center of Mass 
 
-map = [q1,q2,-q2,q2,0,q3,q4,q5,q6];
+dyn.map = [q1,q2,-q2,q2,0,q3,q4,q5,q6];
 % %INPLANE cg_i from frame i
 % for i = 1:length(l_cg)-2 
 % p_cg(i,:)  = T(:,:,i)*DH(map(i),0,0,0)*transpose(l_cg(i,:));
 % end 
 
 %INAXIS cg_i from frame i+1
-for i = 1:length(l_cg)-2 
-p_cg(i,:)  = T(:,:,i+1)*transpose([l_cg(i,:)]);
+for i = 1:length(dyn.l_cg)-2 
+dyn.p_cg(i,:)  = dyn.T(:,:,i+1)*transpose([dyn.l_cg(i,:)]);
 end 
 
 counter_num = 2;
-Tee_cg(:,:,1)= T(:,:,2)*DH(map(2),0,0,0);
-Tee_cg(:,:,2)= T(:,:,2)*DH(-p/2-beta+q2,l_cg(11,1),0,-p/2)*DH(0,0,map(6),0);
+dyn.Tee_cg(:,:,1)= dyn.T(:,:,2)*DH(dyn.map(2),0,0,0);
+dyn.Tee_cg(:,:,2)= dyn.T(:,:,2)*DH(-dyn.p/2-dyn.beta+gen.q(2),dyn.l_cg(11,1),0,-dyn.p/2)*DH(0,0,dyn.map(6),0);
 
- p_cg(end+1,:) = Tee_cg(:,:,1)*transpose(l_cg(10,:)); %Pitch Counterweight 
- p_cg(end+1,:) = Tee_cg(:,:,2)*transpose(l_cg(11,:)); %Insertion Counterweight 
+ dyn.p_cg(end+1,:) = dyn.Tee_cg(:,:,1)*transpose(dyn.l_cg(10,:)); %Pitch Counterweight 
+ dyn.p_cg(end+1,:) = dyn.Tee_cg(:,:,2)*transpose(dyn.l_cg(11,:)); %Insertion Counterweight 
 
  
 %% Plot Joint Angles Test
@@ -262,41 +259,41 @@ Tee_cg(:,:,2)= T(:,:,2)*DH(-p/2-beta+q2,l_cg(11,1),0,-p/2)*DH(0,0,map(6),0);
 %     Jv_cg(1:3,1:6,i)=[diff(T_cg(1:3,4,i),q1),diff(T_cg(1:3,4,i),q2),diff(T_cg(1:3,4,i),q3),diff(T_cg(1:3,4,i),q4),diff(T_cg(1:3,4,i),q5),diff(T_cg(1:3,4,i),q6)];
 % end 
 
- for i=1:length(p_cg)
-     Jv_cg(1:3,1:6,i)=[diff(transpose(p_cg(i,1:3)),q1),diff(transpose(p_cg(i,1:3)),q2),diff(transpose(p_cg(i,1:3)),q3),diff(transpose(p_cg(i,1:3)),q4),diff(transpose(p_cg(i,1:3)),q5),diff(transpose(p_cg(i,1:3)),q6)];
+ for i=1:length(dyn.p_cg)
+     dyn.Jv_cg(1:3,1:6,i)=[diff(transpose(dyn.p_cg(i,1:3)),gen.q(1)),diff(transpose(dyn.p_cg(i,1:3)),gen.q(2)),diff(transpose(dyn.p_cg(i,1:3)),gen.q(3)),diff(transpose(dyn.p_cg(i,1:3)),gen.q(4)),diff(transpose(dyn.p_cg(i,1:3)),gen.q(5)),diff(transpose(dyn.p_cg(i,1:3)),gen.q(6))];
  end 
 
 %Angular Jacobian
 k=[0;0;1];
-Jw_m(1:3,:)=[T(1:3,1:3,1)*k, T(1:3,1:3,4)*k,0 ,T(1:3,1:3,7)*k,T(1:3,1:3,8)*k,T(1:3,1:3,9)*k];
+dyn.Jw_m(1:3,:)=[dyn.T(1:3,1:3,1)*k, dyn.T(1:3,1:3,4)*k,0 ,dyn.T(1:3,1:3,7)*k,dyn.T(1:3,1:3,8)*k,dyn.T(1:3,1:3,9)*k];
 
-Jw(1:3,1:6,1)=zeros(3,6);
-Jw(1:3,1:1,1)=Jw_m(1:3,1);
+dyn.Jw(1:3,1:6,1)=sym(zeros(3,6));
+dyn.Jw(1:3,1:1,1)=dyn.Jw_m(1:3,1);
 
 %Close Kinematic Chain Links
-Jw(1:3,1:6,2)=zeros(3,6);
-Jw(1:3,1:2,2)=[T(1:3,1:3,1)*k, T(1:3,1:3,2)*k];
+dyn.Jw(1:3,1:6,2)=sym(zeros(3,6));
+dyn.Jw(1:3,1:2,2)=[dyn.T(1:3,1:3,1)*k, dyn.T(1:3,1:3,2)*k];
 
-Jw(1:3,1:6,3)=zeros(3,6);
-Jw(1:3,1:2,3)=[T(1:3,1:3,1)*k, -T(1:3,1:3,3)*k];
+dyn.Jw(1:3,1:6,3)=sym(zeros(3,6));
+dyn.Jw(1:3,1:2,3)=[dyn.T(1:3,1:3,1)*k, -dyn.T(1:3,1:3,3)*k];
 
-Jw(1:3,1:6,4)=zeros(3,6);
-Jw(1:3,1:2,4)=[T(1:3,1:3,1)*k, T(1:3,1:3,4)*k];
+dyn.Jw(1:3,1:6,4)=sym(zeros(3,6));
+dyn.Jw(1:3,1:2,4)=[dyn.T(1:3,1:3,1)*k, dyn.T(1:3,1:3,4)*k];
 
-Jw(1:3,1:6,5)=zeros(3,6);
-Jw(1:3,1:2,5)=[T(1:3,1:3,1)*k, T(1:3,1:3,5)*k];
+dyn.Jw(1:3,1:6,5)=sym(zeros(3,6));
+dyn.Jw(1:3,1:2,5)=[dyn.T(1:3,1:3,1)*k, dyn.T(1:3,1:3,5)*k];
 
 for i=3:6
-    Jw(1:3,1:6,i+3)=zeros(3,6);
+    dyn.Jw(1:3,1:6,i+3)=sym(zeros(3,6));
     if(i<7)
-    Jw(1:3,1:i,i+3)=Jw_m(1:3,1:i);
+    dyn.Jw(1:3,1:i,i+3)=dyn.Jw_m(1:3,1:i);
     else
-    Jw(1:3,1:6,i+3)=Jw_m(1:3,1:6);
+    dyn.Jw(1:3,1:6,i+3)=dyn.Jw_m(1:3,1:6);
     end
 end
 
-    Jw(1:3,1:6,10)=Jw(1:3,1:6,2);
-    Jw(1:3,1:6,11)=Jw(1:3,1:6,2);
+    dyn.Jw(1:3,1:6,10)=dyn.Jw(1:3,1:6,2);
+    dyn.Jw(1:3,1:6,11)=dyn.Jw(1:3,1:6,2);
 
 
 %Jw_num(:,:,:)=double((subs(Jw(:,:,:),[q1 q2 q3 q4 q5 q6],q_n)));
@@ -304,78 +301,78 @@ end
 %% Calculate End Link Jacobians
 %Linear Jacobian
 
-for i=1:length(T)
-    Jv(1:3,1:6,i)=[diff(T(1:3,4,i),q1),diff(T(1:3,4,i),q2),diff(T(1:3,4,i),q3),diff(T(1:3,4,i),q4),diff(T(1:3,4,i),q5),diff(T(1:3,4,i),q6)];
+for i=1:length(dyn.T)
+    dyn.Jv(1:3,1:6,i)=[diff(dyn.T(1:3,4,i),gen.q(1)),diff(dyn.T(1:3,4,i),gen.q(2)),diff(dyn.T(1:3,4,i),gen.q(3)),diff(dyn.T(1:3,4,i),gen.q(4)),diff(dyn.T(1:3,4,i),gen.q(5)),diff(dyn.T(1:3,4,i),gen.q(6))];
 end 
 
-J_end = [Jv(:,:,11);Jw(:,:,11)]; %End Effector Jacobian
+gen.J_end = [dyn.Jv(:,:,11);dyn.Jw(:,:,11)]; %End Effector Jacobian
 %% Calculate D Matrix 
 
 
-if dynamic==1
+if dyn.dynamic==1
 
 
 
 %%
-for i=1:length(Jv_cg)
+for i=1:length(dyn.Jv_cg)
    %B(1:9,1:6,i)=M(i)*Jv(:,:,i)'*Jv(:,:,i)+Jw(:,:,i)'*I(1:3,1:3,i)*Jw(:,:,i);
    if (i==5)
-   B(1:6,1:6,i) = zeros(6);
+   dyn.B(1:6,1:6,i) = zeros(6);
    elseif(i==10)
-      B(1:6,1:6,i)=M(i)*Jv_cg(:,:,i)'*Jv_cg(:,:,i)+Jw(:,:,i)'*Tee_cg(1:3,1:3,1)*I(1:3,1:3,i)*Tee_cg(1:3,1:3,1)'*Jw(:,:,i);     
+      dyn.B(1:6,1:6,i)=dyn.M(i)*dyn.Jv_cg(:,:,i)'*dyn.Jv_cg(:,:,i)+dyn.Jw(:,:,i)'*dyn.Tee_cg(1:3,1:3,1)*dyn.I(1:3,1:3,i)*dyn.Tee_cg(1:3,1:3,1)'*dyn.Jw(:,:,i);     
    elseif(i==11)
-      B(1:6,1:6,i)=M(i)*Jv_cg(:,:,i)'*Jv_cg(:,:,i)+Jw(:,:,i)'*Tee_cg(1:3,1:3,2)*I(1:3,1:3,i)*Tee_cg(1:3,1:3,2)'*Jw(:,:,i);     
+      dyn.B(1:6,1:6,i)=dyn.M(i)*dyn.Jv_cg(:,:,i)'*dyn.Jv_cg(:,:,i)+dyn.Jw(:,:,i)'*dyn.Tee_cg(1:3,1:3,2)*dyn.I(1:3,1:3,i)*dyn.Tee_cg(1:3,1:3,2)'*dyn.Jw(:,:,i);     
    else
-       B(1:6,1:6,i)=M(i)*Jv_cg(:,:,i)'*Jv_cg(:,:,i)+Jw(:,:,i)'*T(1:3,1:3,i)*I(1:3,1:3,i)*T(1:3,1:3,i)'*Jw(:,:,i); %Not sure about transformations
+       dyn.B(1:6,1:6,i)=dyn.M(i)*dyn.Jv_cg(:,:,i)'*dyn.Jv_cg(:,:,i)+dyn.Jw(:,:,i)'*dyn.T(1:3,1:3,i)*dyn.I(1:3,1:3,i)*dyn.T(1:3,1:3,i)'*dyn.Jw(:,:,i); %Not sure about transformations
    end 
 end
 
-D = zeros(dof);
+dyn.D = zeros(gen.dof);
 for i=1:(11)
-    if 6<i&&i<10 %%%DONT FORGET THIS RADIAN, turning off wrist dynamics
-        boo=0;
-    else
-    D = D + B(1:dof,1:dof,i);
-    end
+    %if 6<i&&i<10 %%%DONT FORGET THIS RADIAN, turning off wrist dynamics
+     %   boo=0;
+    %else
+    dyn.D = dyn.D + dyn.B(1:gen.dof,1:gen.dof,i);
+    %end
 end 
 
 
 %D=combine(D,'sincos');
-D = simplify(D);
-D = combine(D);
+dyn.D = simplify(dyn.D);
+dyn.D = combine(dyn.D);
 
 %% Crystoffel Symbols
-for i=1:dof
-    for j=1:dof
-        for k=1:dof
-            c(i,j,k)=diff(D(k,j),Q(i))+diff(D(k,i),Q(j))-diff(D(i,j),Q(k));
+for i=1:gen.dof
+    for j=1:gen.dof
+        for k=1:gen.dof
+            dyn.c(i,j,k)=diff(dyn.D(k,j),gen.Q(i))+diff(dyn.D(k,i),gen.Q(j))-diff(dyn.D(i,j),gen.Q(k));
         end
     end
 end
 
-C=sym(zeros(dof,dof));
-    for j=1:dof
-        for k=1:dof
-            for i=1:dof
-                C(k,j)= C(k,j) + c(i,j,k)*Qd(i);
+dyn.C=sym(zeros(gen.dof,gen.dof));
+    for j=1:gen.dof
+        for k=1:gen.dof
+            for i=1:gen.dof
+                dyn.C(k,j)= dyn.C(k,j) + dyn.c(i,j,k)*gen.Qd(i);
                 %C(k,j)=(c(1,j,k)*Qd(1)+c(2,j,k)*Qd(2)+c(3,j,k)*Qd(3)+c(4,j,k)*Qd(4)+c(5,j,k)*Qd(5)+c(6,j,k)*Qd(6));
             end
         end
     end
     
 %%  Potential Energy
-  P=sym(zeros(1,length(p_cg)));
-  P_tog=sym(zeros(1));
-  Psi = sym(zeros(dof,1));
+  dyn.P=sym(zeros(1,length(dyn.p_cg)));
+  dyn.P_tog=sym(zeros(1));
+  dyn.Psi = sym(zeros(gen.dof,1));
   
-  for i=1:length(p_cg)
-  P(i) = g*M(i)*p_cg(i,3);
+  for i=1:length(dyn.p_cg)
+  dyn.P(i) = dyn.g*dyn.M(i)*dyn.p_cg(i,3);
   
   if (i==5)
-      P(i)=0;
+      dyn.P(i)=0;
   end
   
-  P_tog = P_tog + P(i);
+  dyn.P_tog = dyn.P_tog + dyn.P(i);
   
   end 
   
@@ -395,40 +392,40 @@ C=sym(zeros(dof,dof));
 %   end 
   
   
-for i=1:dof 
-Psi(i,1)=diff(P_tog,Q(i));
+for i=1:gen.dof 
+dyn.Psi(i,1)=diff(dyn.P_tog,gen.Q(i));
 end
 
 %% Frictiones 
-Fv=sym('Fv_%d',[6 1],'real');
-Fvl=sym('Fvl_%d%d',[2 2],'real');
+dyn.Fv=sym('Fv_%d',[6 1],'real');
+dyn.Fvl=sym('Fvl_%d%d',[2 2],'real');
 
-M_Fv = diag(Fv);
-M_Fv(5:6,5:6)=Fvl;
+dyn.M_Fv = diag(dyn.Fv);
+dyn.M_Fv(5:6,5:6)=dyn.Fvl;
 
-Fs = sym('Fs_%d',[6 1],'real');
-M_Fs = diag(Fs);
+dyn.Fs = sym('Fs_%d',[6 1],'real');
+dyn.M_Fs = diag(dyn.Fs);
 
-Ke = sym('Ke_%d',[6 1], 'real');
+dyn.Ke = sym('Ke_%d',[6 1], 'real');
 
-M_Ke = diag(Ke);
-M_Ke(3,3)= 0;
-M_Ke(5,5)= 0;
-M_Ke(6,6)= 0;
+dyn.M_Ke = diag(dyn.Ke);
+dyn.M_Ke(3,3)= 0;
+dyn.M_Ke(5,5)= 0;
+dyn.M_Ke(6,6)= 0;
 
 %% Put together
 
-Dt=D(1:dof,1:dof)*Qdd(1:dof)+C(1:dof,1:dof)*Qd(1:dof)+Psi(1:dof,:);
+dyn.Dt=dyn.D(1:gen.dof,1:gen.dof)*gen.Qdd(1:gen.dof)+dyn.C(1:gen.dof,1:gen.dof)*gen.Qd(1:gen.dof)+dyn.Psi(1:gen.dof,:);
 
-Dt=Dt-M_Fv(1:dof,1:dof)*Qd(1:dof)-M_Fs(1:dof,1:dof)*sign(Qd(1:dof))-M_Ke(1:dof,1:dof)*Q(1:dof);
-Dt=simplify(Dt);
+dyn.Dt=dyn.Dt-dyn.M_Fv(1:gen.dof,1:gen.dof)*gen.Qd(1:gen.dof)-dyn.M_Fs(1:gen.dof,1:gen.dof)*sign(gen.Qd(1:gen.dof))-dyn.M_Ke(1:gen.dof,1:gen.dof)*gen.Q(1:gen.dof);
+dyn.Dt=simplify(dyn.Dt);
 
-temp=strcat(filename,'_temp_sim.mat');
+temp=strcat(gen.filename,'_temp_sim.mat');
 save(temp);
 %T=combine(T,'sincos');
 
 %Dt=collect(Dt,[Ixx,Ixy,Ixz,Iyy,Iyz,Izz, lc1, lc2, lc3, lc4, lc5, lc6, lc7, lc8, lc9,m1, m2, m3, m4, m5, m6, m7, m8, m9]);
-Dt=collect(Dt,[Ixx,Ixy,Ixz,Iyy,Iyz,Izz,reshape(l_cg,[1,numel(l_cg)]),M,Fv,reshape(Fvl,1,[]),Fs,Ke]);
+dyn.Dt=collect(dyn.Dt,[dyn.Ixx,dyn.Ixy,dyn.Ixz,dyn.Iyy,dyn.Iyz,dyn.Izz,reshape(dyn.l_cg,[1,numel(dyn.l_cg)]),dyn.M,dyn.Fv,reshape(dyn.Fvl,1,[]),dyn.Fs,dyn.Ke]);
 
 % for i=1:length(p_cg)
 %     X1_X2_mX(i) = sprintf('l%d_1_l%d_2_m%d',[i i i]);
@@ -438,20 +435,20 @@ Dt=collect(Dt,[Ixx,Ixy,Ixz,Iyy,Iyz,Izz,reshape(l_cg,[1,numel(l_cg)]),M,Fv,reshap
 % end
 % 
 
-lclcm = sym('lc%d_%d_lc%d_%d_m%d', [num 3 num 3 num]);
-lclcm_sym= sym(zeros(num,3,num,3,num));
+dyn.lclcm = sym('lc%d_%d_lc%d_%d_m%d', [dyn.num 3 dyn.num 3 dyn.num]);
+dyn.lclcm_sym= sym(zeros(dyn.num,3,dyn.num,3,dyn.num));
 
-for i=1:num
+for i=1:dyn.num
     for j=1:3
-        for k=1:num
+        for k=1:dyn.num
             for l=1:3
-                for m=1:num
+                for m=1:dyn.num
                     if(k<i||l<j)
-                        lclcm(i,j,k,l,m) = 0;
+                        dyn.lclcm(i,j,k,l,m) = 0;
 %                     elseif(l>j)
 %                         lclcm_sym(i,j,k,l,m) = 0;
                     else
-                    lclcm_sym(i,j,k,l,m)=l_cg(i,j)*l_cg(k,l)*M(m);
+                    dyn.lclcm_sym(i,j,k,l,m)=dyn.l_cg(i,j)*dyn.l_cg(k,l)*dyn.M(m);
                     end
                 end
             end
@@ -459,27 +456,27 @@ for i=1:num
     end
 end
 
-lcm = sym('lc%d_%d_m%d', [num 3 num]);
-lcm_sym= sym(zeros(num,3,num));
+dyn.lcm = sym('lc%d_%d_m%d', [dyn.num 3 dyn.num]);
+dyn.lcm_sym= sym(zeros(dyn.num,3,dyn.num));
 
-for i=1:num
+for i=1:dyn.num
     for j=1:3
-        for m=1:num
-            lcm_sym(i,j,m)=l_cg(i,j)*M(m);
+        for m=1:dyn.num
+            dyn.lcm_sym(i,j,m)=dyn.l_cg(i,j)*dyn.M(m);
         end
     end
 end
 
 
-lclcm = reshape(lclcm,1,[]);
-lclcm_sym = reshape(lclcm_sym,1,[]);
+dyn.lclcm = reshape(dyn.lclcm,1,[]);
+dyn.lclcm_sym = reshape(dyn.lclcm_sym,1,[]);
 
-lcm = reshape(lcm,1,[]);
-lcm_sym = reshape(lcm_sym,1,[]);
+dyn.lcm = reshape(dyn.lcm,1,[]);
+dyn.lcm_sym = reshape(dyn.lcm_sym,1,[]);
 
 
-Dt = subs(Dt, lclcm_sym,lclcm);
-Dt = subs(Dt, lcm_sym,lcm);
+dyn.Dt = subs(dyn.Dt, dyn.lclcm_sym,dyn.lclcm);
+dyn.Dt = subs(dyn.Dt, dyn.lcm_sym,dyn.lcm);
 
 
 % lcX_2_mX = reshape(lcX_2_mX,1,[]);
@@ -501,41 +498,41 @@ Dt = subs(Dt, lcm_sym,lcm);
 % Dt = subs(Dt, LcX2_mX_mult,lcX_2_mX);
 % 
 % Dt = subs(Dt, LcX_mX_mult ,lcX_mX);
-temp=strcat(filename,'_temp_subs.mat');
+temp=strcat(gen.filename,'_temp_subs.mat');
 save(temp)
 
-if dof<6
-Dt = subs(Dt, q(dof+1:6),zeros(1,6-dof));
+if gen.dof<6
+dyn.Dt = subs(dyn.Dt, gen.q(gen.dof+1:6),zeros(1,6-gen.dof));
 end
 
 %% Regressor Matrix Form
-Par=symvar(Dt);
+gen.Par=symvar(dyn.Dt);
 
 %Delete q's from Par
-check=q;
-check(end+1:end+length(qd)) = qd;
-check(end+1:end+length(qdd)) = qdd;
-[bullshit,ind]=ismember(check,Par);
+check=gen.q;
+check(end+1:end+length(gen.qd)) = gen.qd;
+check(end+1:end+length(gen.qdd)) = gen.qdd;
+[bullshit,ind]=ismember(check,gen.Par);
 
  for k=length(ind):-1:1
        if ind(k)~= 0
-       Par(ind(k))=[];
+       gen.Par(ind(k))=[];
        end
  end
  
  
 %Par(end-17:end)=[]; 
-[Y, tau]=equationsToMatrix(Dt == Tau(1:dof), Par);
+[gen.Y, tau]=equationsToMatrix(dyn.Dt == gen.Tau(1:gen.dof), gen.Par);
 
 %% Lumping Parameters
-cond_ys2=100000;
- old = cond_ys2;
-while cond_ys2>200
+gen.cond_ys2_lump=100000;
+ old = gen.cond_ys2_lump;
+while gen.cond_ys2_lump>200
     
 
 %finding the linear combinations
 %[Ys1, Ys2, Par1, Par2, cond_ys2, W]=lumping_parameters_new(Y,Par,dof);
-[Ys2,Par2,cond_ys2,W]=svd_reduction(Y,Par,dof);
+[gen.Ys2,gen.Par2,gen.cond_ys2_lump,W]=svd_reduction(gen.Y,gen.Par,gen.dof);
 % %% Trajectory Optimization 
 % 
 % [x, v, cond_save]=traj_opt_rand(Ys2,size(Ys2,2));
@@ -546,14 +543,14 @@ disp('Resulting Regressor Matrix: ')
 %Ys2
 
 disp('Resulting Identifiable Parameters: ')
-length(Par2)
-if old>cond_ys2
-temp=strcat(filename,'_all.mat');
+length(gen.Par2)
+if old>gen.cond_ys2_lump
+temp=strcat(gen.filename,'_all.mat');
 save(temp)
 
-temp2=strcat(filename,'_Y.mat');
-save(temp2,'Ys2','Par2','Q','Qd','Qdd')
- old = cond_ys2;
+temp2=strcat(gen.filename,'_Y.mat');
+save(temp2,'gen')
+ old = gen.cond_ys2_lump;
 end
 end
 

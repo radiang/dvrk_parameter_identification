40e6966a816e8aece4c5086673a610b94678d40a
adnan <radian>
2018-04-28 19:58:15 -0400

    fixed impedence control code

--- psm_kinematics/dynamics_derivation/impedance_control_run.m
+++ psm_kinematics/dynamics_derivation/impedance_control_run.m
@@ -15,7 +15,7 @@ qdt = diff(qt,t);
 qddt = diff(qdt,t);
 
 %% Options
-debug = 1;
+debug = 0;
 
 %% Jacobian End Effector and it's Derivative
 J_end_t = subs(gen.J_end, symvar(gen.J_end), qt);
@@ -40,7 +40,7 @@ J3_num = subs(J3_num,q3t,q_n(3));
 
 J3_num = double(J3_num);
 
-figure()
+
 % for i = 1:length(T)
 %         T_num(:,:,i)=subs(T(:,:,i),[q1 q2 q3 q4 q5 q6],q_n);
 %         
@@ -52,38 +52,53 @@ figure()
 % end
 
 
-T_num(:,:,:)=double(subs(dyn.T(:,:,:),[gen.q(1:6)],q_n));
-a(1,:) = reshape(T_num(1,4,:),1,[]);
-a(2,:) = reshape(T_num(2,4,:),1,[]);
-a(3,:) = reshape(T_num(3,4,:),1,[]);
-scatter3(a(1,:),a(2,:),a(3,:));
-line(a(1,:),a(2,:),a(3,:));
-hold on
-
-x0 = double(subs(dyn.T(1:3,4,11), gen.q, q_n));
-
-xv=J3_num*transpose([10 0 0]);
-xvx=xv
-xd = x0+xv(1:3);
-plot3([xd(1),T_num(1,4,11)],[xd(2),T_num(2,4,11)],[xd(3),T_num(3,4,11)],'r');
-hold on
-xv=J3_num*transpose([0 10 0]);
-xvy=xv
-xd = x0+xv(1:3);
-plot3([xd(1),T_num(1,4,11)],[xd(2),T_num(2,4,11)],[xd(3),T_num(3,4,11)],'g');
-xv=J3_num*transpose([0 0 0.5]);
-xd = x0+xv(1:3);
-plot3([xd(1),T_num(1,4,11)],[xd(2),T_num(2,4,11)],[xd(3),T_num(3,4,11)],'b');
 
 
-title('Plot transform Frames');
-xlabel('x');
-ylabel('y');
-zlabel('z');
-s=1;
-xlim([-s/2 s])
-ylim([-s/2 s])
-zlim([-s/2 s])
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%figure()
+% T_num(:,:,:)=double(subs(dyn.T(:,:,:),[gen.q(1:6)],q_n));
+% a(1,:) = reshape(T_num(1,4,:),1,[]);
+% a(2,:) = reshape(T_num(2,4,:),1,[]);
+% a(3,:) = reshape(T_num(3,4,:),1,[]);
+% scatter3(a(1,:),a(2,:),a(3,:));
+% line(a(1,:),a(2,:),a(3,:));
+% hold on
+% 
+% x0 = double(subs(dyn.T(1:3,4,11), gen.q, q_n));
+% 
+% xv=J3_num*transpose([10 0 0]);
+% xvx=xv
+% xd = x0+xv(1:3);
+% plot3([xd(1),T_num(1,4,11)],[xd(2),T_num(2,4,11)],[xd(3),T_num(3,4,11)],'r');
+% hold on
+% xv=J3_num*transpose([0 10 0]);
+% xvy=xv
+% xd = x0+xv(1:3);
+% plot3([xd(1),T_num(1,4,11)],[xd(2),T_num(2,4,11)],[xd(3),T_num(3,4,11)],'g');
+% xv=J3_num*transpose([0 0 0.5]);
+% xd = x0+xv(1:3);
+% plot3([xd(1),T_num(1,4,11)],[xd(2),T_num(2,4,11)],[xd(3),T_num(3,4,11)],'b');
+% 
+% 
+% title('Plot transform Frames');
+% xlabel('x');
+% ylabel('y');
+% zlabel('z');
+% s=1;
+% xlim([-s/2 s])
+% ylim([-s/2 s])
+% zlim([-s/2 s])
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+
+
 % 
 % for i = 1:length(T_cg)
 %         T_cg_num(:,:,i)=subs(T_cg(:,:,i),[q1 q2 q3 q4 q5 q6 lc1 lc2 lc3 lc4 lc6 lc7 lc8 lc9], [q_n lc_n]);
@@ -129,73 +144,69 @@ ctrl.Nu3 = subs(Nu(1:3,1),[transpose(gen.Q(4:6)),transpose(gen.Qd(4:6))],[0, 0, 
  stringname = strcat('ccode/',gen.filename,'_Ys2_ccode.c');
  ccode(gen.Ys2,'File',stringname,'Comments','V1.2');
 %% Substitute to continuous time
-D_t = subs(dyn.D, [q, qd, qdd], [qt, qdt, qddt]);
-C_t = subs(dyn.C, [q, qd, qdd], [qt, qdt, qddt]);
-Psi_t = subs(dyn.Psi, [q, qd, qdd], [qt, qdt, qddt]);
+D_t = subs(dyn.D, [gen.q, gen.qd, gen.qdd], [qt, qdt, qddt]);
+C_t = subs(dyn.C, [gen.q, gen.qd, gen.qdd], [qt, qdt, qddt]);
+Psi_t = subs(dyn.Psi, [gen.q, gen.qd, gen.qdd], [qt, qdt, qddt]);
 
 
 %% Fill up Inertias and Masses (From Solidworks)
 m_num = 0.5*ones(1,11);
 lc_num = 0.02 *ones(1,9);
 Ic_num = 0.01 * ones(1,11*6);
-l_cg_num = 0.02*ones(1,numel(l_cg(:,1:3)));
+l_cg_num = 0.02*ones(1,numel(dyn.l_cg(:,1:3)));
 
-%eqn = D_t(1:3,1:3) * qddt(1:3)' + C_t(1:3,1:3)*qdt(1:3)'+ Psi_t(1:3);
-%temp = symvar(eqn);
+%% Actual values of the PSM from Solidworks
+psm.m = [1.47048, 0.995109+0.0744253, 0.178407+0.445695, 2.09102, 0, 0.0311806 + 0.193728, 0.000342894,0.000249121,6.63661e-06, 0.995109+0.0744253, 0.0311806 + 0.193728 ]; %Kg
+psm.I(:,:,1) = [ 0.01571450, 0.0000043 , 0.00000728; 0, 0.01814321, -0.00048589; 0, 0, 0.00824930];  %kg m^2 
+psm.I(:,:,2) = [ 0.00202417+0.00000876, 0.00047511 , 0.00000043-0.00001711; 0, 0.00564963+0.00022654, 0.00000129; 0, 0, 0.00564716+0.00022322];  %pitch back and front added together
+psm.I(:,:,3) = [ 0.00463460+0.01127103, 0.00001128-0.00031801 , 0.00000003; 0,0.00014989+0.00045979, -0.00000040; 0, 0, 0.00449516+0.01110239];  %pitch top and bottom added together
+psm.I(:,:,4) = [ 0.00156905, -0.00123970, 0.00000002; 0, 0.04284466, 0.0000; 0, 0, 0.04227453];  %kg m^2 
+psm.I(:,:,5) = zeros(3);  
+psm.I(:,:,6) = [ 0.00006363 + 0.00139739, 0.00000014, -0.00000018-0.00000957; 0, 000004346+0.00135923, 0.00000157; 0, 0, 0.00002063+0.00006656];  %kg m^2 
+psm.I(:,:,7) = zeros(3);  %kg m^2 
+psm.I(:,:,8) = zeros(3);  %kg m^2 
+psm.I(:,:,9) = zeros(3);  %kg m^2 
+psm.I(:,:,10) = psm.I(:,:,2); %Asumptions
+psm.I(:,:,11) = psm.I(:,:,6); %Asumptions
 
-% %% Actual values of the PSM from Solidworks
-% psm.m = [1.47048, 0.995109+0.0744253, 0.178407+0.445695, 2.09102, 0, 0.0311806 + 0.193728, 0.000342894,0.000249121,6.63661e-06, 0.995109+0.0744253, 0.0311806 + 0.193728 ]; %Kg
-% psm.I(:,:,1) = [ 0.01571450, 0.0000043 , 0.00000728; 0, 0.01814321, -0.00048589; 0, 0, 0.00824930];  %kg m^2 
-% psm.I(:,:,2) = [ 0.00202417+0.00000876, 0.00047511 , 0.00000043-0.00001711; 0, 0.00564963+0.00022654, 0.00000129; 0, 0, 0.00564716+0.00022322];  %pitch back and front added together
-% psm.I(:,:,3) = [ 0.00463460+0.01127103, 0.00001128-0.00031801 , 0.00000003; 0,0.00014989+0.00045979, -0.00000040; 0, 0, 0.00449516+0.01110239];  %pitch top and bottom added together
-% psm.I(:,:,4) = [ 0.00156905, -0.00123970, 0.00000002; 0, 0.04284466, 0.0000; 0, 0, 0.04227453];  %kg m^2 
-% psm.I(:,:,5) = zeros(3);  
-% psm.I(:,:,6) = [ 0.00006363 + 0.00139739, 0.00000014, -0.00000018-0.00000957; 0, 000004346+0.00135923, 0.00000157; 0, 0, 0.00002063+0.00006656];  %kg m^2 
-% psm.I(:,:,7) = zeros(3);  %kg m^2 
-% psm.I(:,:,8) = zeros(3);  %kg m^2 
-% psm.I(:,:,9) = zeros(3);  %kg m^2 
-% psm.I(:,:,10) = psm.I(:,:,2); %Asumptions
-% psm.I(:,:,11) = psm.I(:,:,6); %Asumptions
-% 
-% %Still assumption
-% psm.l_cg(1,:) = [   0   ,  0.038769    ,0.158996 ]; %x -y -z
-% psm.l_cg(2,:) = [   -0.012175*cos(beta)-0.036897*sin(beta)  ,  +0.012175*sin(beta)-0.036897*cos(beta)    ,0]; %
-% psm.l_cg(3,:) = [   0.010348*cos(beta)+0.256832*sin(beta)  ,-0.010348*sin(beta)+0.256832*cos(beta), 0 ]; %
-% psm.l_cg(4,:) = [ -0.001929  ,-0.136127 , 0 ]; %
-% psm.l_cg(5,:) = [   0  , 0 , 0 ]; %
-% psm.l_cg(6,:) = [   0.001528 ,-0.000127 , 0.013848]; %
-% 
-% psm.l_cg(11,:) = [   0 ,0 , 0]; %
-% 
-% %Make a Symmetric Matrix 
-% % psm.I=(psm.I + psm.I.')/2;
-% 
-% for i = 1:length(psm.I)
-% psm.Ixx(i)=psm.I(1,1,i);
-% psm.Ixy(i)=psm.I(1,2,i);
-% psm.Ixz(i)=psm.I(1,3,i);
-% psm.Iyy(i)=psm.I(2,2,i);
-% psm.Iyz(i)=psm.I(2,3,i);
-% psm.Izz(i)=psm.I(3,3,i);
-% end
+%Still assumption
+psm.l_cg(1,:) = [   0   ,  0.038769    ,0.158996 ]; %x -y -z
+psm.l_cg(2,:) = [   -0.012175*cos(dyn.beta)-0.036897*sin(dyn.beta)  ,  +0.012175*sin(dyn.beta)-0.036897*cos(dyn.beta)    ,0]; %
+psm.l_cg(3,:) = [   0.010348*cos(dyn.beta)+0.256832*sin(dyn.beta)  ,-0.010348*sin(dyn.beta)+0.256832*cos(dyn.beta), 0 ]; %
+psm.l_cg(4,:) = [ -0.001929  ,-0.136127 , 0 ]; %
+psm.l_cg(5,:) = [   0  , 0 , 0 ]; %
+psm.l_cg(6,:) = [   0.001528 ,-0.000127 , 0.013848]; %
+
+psm.l_cg(11,:) = [   0 ,0 , 0]; %
+
+%Make a Symmetric Matrix 
+% psm.I=(psm.I + psm.I.')/2;
+
+for i = 1:length(psm.I)
+psm.Ixx(i)=psm.I(1,1,i);
+psm.Ixy(i)=psm.I(1,2,i);
+psm.Ixz(i)=psm.I(1,3,i);
+psm.Iyy(i)=psm.I(2,2,i);
+psm.Iyz(i)=psm.I(2,3,i);
+psm.Izz(i)=psm.I(3,3,i);
+end
 
 %% Substitution
-D_t = subs(D_t, [ reshape(l_cg(:,1:3),1,[]),transpose(Ixx), transpose(Ixy), transpose(Ixz), transpose(Iyy), transpose(Iyz), transpose(Izz), M ] ,[reshape(psm.l_cg(:,1:3),1,[]),psm.Ixx,psm.Ixy,psm.Ixz,psm.Iyy,psm.Iyz,psm.Izz, psm.m]);
-C_t = subs(C_t, [ reshape(l_cg(:,1:3),1,[]),transpose(Ixx), transpose(Ixy), transpose(Ixz), transpose(Iyy), transpose(Iyz), transpose(Izz), M ] ,[reshape(psm.l_cg(:,1:3),1,[]),psm.Ixx,psm.Ixy,psm.Ixz,psm.Iyy,psm.Iyz,psm.Izz, psm.m]);
-Psi_t = subs(Psi_t, [ reshape(l_cg(:,1:3),1,[]),transpose(Ixx), transpose(Ixy), transpose(Ixz), transpose(Iyy), transpose(Iyz), transpose(Izz), M ] ,[reshape(psm.l_cg(:,1:3),1,[]),psm.Ixx,psm.Ixy,psm.Ixz,psm.Iyy,psm.Iyz,psm.Izz, psm.m]);
+D_t = subs(D_t, [ reshape(dyn.l_cg(:,1:3),1,[]),transpose(dyn.Ixx), transpose(dyn.Ixy), transpose(dyn.Ixz), transpose(dyn.Iyy), transpose(dyn.Iyz), transpose(dyn.Izz), dyn.M ] ,[reshape(psm.l_cg(:,1:3),1,[]),psm.Ixx,psm.Ixy,psm.Ixz,psm.Iyy,psm.Iyz,psm.Izz, psm.m]);
+C_t = subs(C_t, [ reshape(dyn.l_cg(:,1:3),1,[]),transpose(dyn.Ixx), transpose(dyn.Ixy), transpose(dyn.Ixz), transpose(dyn.Iyy), transpose(dyn.Iyz), transpose(dyn.Izz), dyn.M ] ,[reshape(psm.l_cg(:,1:3),1,[]),psm.Ixx,psm.Ixy,psm.Ixz,psm.Iyy,psm.Iyz,psm.Izz, psm.m]);
+Psi_t = subs(Psi_t, [ reshape(dyn.l_cg(:,1:3),1,[]),transpose(dyn.Ixx), transpose(dyn.Ixy), transpose(dyn.Ixz), transpose(dyn.Iyy), transpose(dyn.Iyz), transpose(dyn.Izz), dyn.M ] ,[reshape(psm.l_cg(:,1:3),1,[]),psm.Ixx,psm.Ixy,psm.Ixz,psm.Iyy,psm.Iyz,psm.Izz, psm.m]);
 
 %eqn = D_t(1:3,1:3) * qddt(1:3)' + C_t(1:3,1:3)*qdt(1:3)'+ Psi_t(1:3);
-
 eqn = D_t(1:3,1:3) * transpose(qddt(1:3));
 
-ctrl.xe = subs(T(1:3,4,11), q, qt);
+ctrl.xe = subs(dyn.T(1:3,4,11), gen.q, qt);
 ctrl.xe_d = diff(ctrl.xe,t);
 ctrl.xe_dd= diff(ctrl.xe,t,t);
 
 
 %% Force Controller in x || R^3
 ctrl.Ja=J_end_t(1:3,1:3);
-ctrl.Jd=J_diff_t(1:3,1:3);
+ctrl.Jd=ctrl.J_diff_t(1:3,1:3);
 %Set up Environment
 %Initial Condition
 joint.qt = qt;
@@ -219,7 +230,7 @@ ctrl.Kp = 100*eye(3);
 qd0 = [0.0001 ;0.00001 ;0.00001;0;0;0];
 q0 = [0 ;0 ;0.1;0;0;0];
 
-init.x0n = double(subs(T(1:3,4,11), q, q0.'));
+init.x0n = double(subs(dyn.T(1:3,4,11), gen.q, q0.'));
 
 %Desired Set Position
 syms xd fd
@@ -241,7 +252,7 @@ ctrl.ha=ctrl.he; %assume geo Jac same as ana Jac
 
 
 % Impedence Control with desired force tracking
-u = impedence_control(ctrl,joint,init);
+ctrl.u = impedence_control(ctrl,joint,init);
 
 save('temp2.mat');
 
@@ -250,7 +261,7 @@ if (debug==0)
     
 %% Make ODE Function 
 
-dyn_eqn=u-eqn;
+dyn_eqn=ctrl.u-eqn;
 
 dyn_eqn=simplify(dyn_eqn);
 %dyn_eqn = eqn==u;
@@ -271,20 +282,20 @@ Mfun = odeFunction(Mass,vars,'File','myfileM','Comments','Version: 1.1');
 Ffun = odeFunction(F,vars,'File','myfileF','Comments','Version: 1.1');
 opt = odeset('mass', Mfun, 'InitialSlope', qd0(1:3));
 
-[time, q_out] = ode45(Ffun, [0 7], [q0(1:3);qd0(1:3)], opt);
+[time, ctrl.q_out] = ode45(Ffun, [0 7], [q0(1:3);qd0(1:3)], opt);
 
 
 %% Plot Output
 beg = 1;
-ends = length(q_out);
+ends = length(ctrl.q_out);
 
-T_out = subs(T(1:3,4,11),[q4 q5 q6],[0 0 0]);
+ctrl.T_out = subs(dyn.T(1:3,4,11),[gen.q(4) gen.q(5) gen.q(6)],[0 0 0]);
 
 for i = beg:ends
         %output(1:3,i)=subs(T(1:3,4,11),[q1 q2 q3 q4 q5 q6],[q_out(i,1:3) 0 0 0]);
-        q1 = q_out(i,1);
-        q2 = q_out(i,2);
-        q3 = q_out(i,3);
+        q1 = ctrl.q_out(i,1);
+        q2 = ctrl.q_out(i,2);
+        q3 = ctrl.q_out(i,3);
         
         output(1,i)=               0.15*sin(q2 - 0.29) + 0.043*cos(q2) - 0.15*sin(q2) - 1.0*q3*sin(q2) + 0.49;
         output(2,i)=-2.0e-4*sin(q1)*(744.0*cos(q2) - 755.0*cos(q2 - 0.29) + 222.0*sin(q2) + 5.0e3*q3*cos(q2));
@@ -297,23 +308,23 @@ for i = beg:ends
 end
 
 
-figure()
-        %scatter3(output(1,beg:ends),output(2,beg:ends),output(3,beg:ends));
-        line(output(1,beg:ends),output(2,beg:ends),output(3,beg:ends));
-        hold on
-%         
-        scatter3(output(1,beg),output(2,beg),output(3,beg),'r');     
-        hold on
+% figure()
+%         %scatter3(output(1,beg:ends),output(2,beg:ends),output(3,beg:ends));
+%         line(output(1,beg:ends),output(2,beg:ends),output(3,beg:ends));
+%         hold on
+% %         
+%         scatter3(output(1,beg),output(2,beg),output(3,beg),'r');     
+%         hold on
 %         output(1:3)=subs(T(1:3,4,11),[q1 q2 q3 q4 q5 q6],[q_out(end,1:3) 0 0 0]);
 %         scatter3(output(1),output(2),output(3));
 %         marker_id = sprintf('%d',i);
 %         text(output(1),output(2),output(3),marker_id);     
 %         hold on
 
-title('Plot PSM Force output');
-xlabel('x');
-ylabel('y');
-zlabel('z');
+% title('Plot PSM Force output');
+% xlabel('x');
+% ylabel('y');
+% zlabel('z');
 
 end
 end
